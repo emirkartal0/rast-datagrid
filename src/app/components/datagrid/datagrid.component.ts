cc8cfa45f45bad69a6327d63b7e17bb631e63380
emir <emirkartal00@gmail.com>
2024-02-07 00:55:37 +0300

    feat: pagination logic & ui

--- src/app/components/datagrid/datagrid.component.ts
+++ src/app/components/datagrid/datagrid.component.ts
@@ -4,11 +4,12 @@ import { AccountData } from '../../models/account-data.interface';
 import { NotificationService } from '../../services/notification.service';
 import { AddAccountModalComponent } from '../add-account-modal/add-account-modal.component';
 import { LocalStorageService } from '../../services/local-storage.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'datagrid',
     standalone: true,
-    imports: [CommonModule, AddAccountModalComponent],
+    imports: [CommonModule, AddAccountModalComponent, FormsModule],
     templateUrl: './datagrid.component.html',
     styleUrl: './datagrid.component.css'
 })
@@ -16,6 +17,19 @@ export class DatagridComponent {
 
     rowNumber: number = 4;
     openModal: boolean = false;
+    currentPage: number = 1;
+
+    get accountDatas(): AccountData[] {
+        return this.localStorageService.getSocialDatas();
+    }
+
+    get paginatedDatas(): AccountData[] {
+        return this.accountDatas.slice((this.currentPage - 1) * this.rowNumber, this.currentPage * this.rowNumber);
+    }
+
+    get totalPages(): number {
+        return Math.ceil(this.accountDatas.length / this.rowNumber);
+    }
 
     constructor(
         private notificationService: NotificationService,
@@ -30,15 +44,9 @@ export class DatagridComponent {
         this.rowNumber += number;
     }
 
-    get accountDatas (): AccountData[] {
-        return this.localStorageService.getSocialDatas();
+    changePageNumber(number: number) {
+        if (this.currentPage + number > 5 || this.currentPage + number < 1) return;
+        this.currentPage += number;
     }
-
-    socialDatas: AccountData[] = [
-        {url: 'https://www.facebook.com', name: 'Facebook', description: 'Facebook is a social networking site that makes it easy for you to connect and share with your family and friends online.'},
-        {url: 'https://www.twitter.com', name: 'Twitter', description: 'Twitter is an online news and social networking site where people communicate in short messages called tweets.'},
-        {url: 'https://www.linkedin.com', name: 'Linkedin', description: 'LinkedIn is a business-oriented social networking service.'},
-        {url: 'https://www.instagram.com', name: 'Instagram', description: 'Instagram is a fun and quirky way to share your life with friends through a series of pictures.'},
-    ];
 
 }
