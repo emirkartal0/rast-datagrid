f977d9a4cac838a61506bd1171746b2421380f68
emir <emirkartal00@gmail.com>
2024-02-06 14:19:59 +0300

    feat: add responsive navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'navbar',
+    standalone: true,
+    imports: [],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+    
+    isMenuOpen = false;
+
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
 
 }
